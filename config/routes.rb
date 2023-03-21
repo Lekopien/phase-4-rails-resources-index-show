423f6d6e4c93b42471a1ea8d29df1d5094592a7f
lekopien <mavericklekopien@gmail.com>
2023-03-21 23:09:35 +0300

    Tools like resources-use of the convention over configuration paradigm.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get '/birds', to: 'birds#index'
+  # get '/birds/:id', to: 'birds#show'
+
+  # resources :birds--Rails provides a tool, resources, that enforces RESTful conventions and saves us work.
+# index
+# create
+# show
+# update
+# update
+# destroy
+resources :birds, only: [:index, :show]
 end
